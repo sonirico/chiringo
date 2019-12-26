e1f00327b6580aa7c4a4db91f0e3c91e556178cf
Marcos <marcos.sanchez@etoshi.io>
2019-12-26 18:19:33 +0100

    feat(chain): mine block endpoint

--- handlers.go
+++ handlers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 	"strconv"
@@ -18,11 +19,28 @@ func (n *Node) GetBlock(w http.ResponseWriter, req *http.Request) {
 	blockIndexParam := vars["index"]
 	index, err := strconv.Atoi(blockIndexParam)
 	if err != nil || index < 0 || index >= n.chain.Size() {
-		w.WriteHeader(http.StatusNotFound)
+		http.NotFound(w, req)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(n.chain.elements[index])
+}
+
+func (n *Node) MineBlock(w http.ResponseWriter, req *http.Request) {
+	type payload struct {
+		Data string `json:"data"`
+	}
+	var pay payload
+	err := json.NewDecoder(req.Body).Decode(&pay)
+	if err != nil {
+		http.Error(w, "invalid payload", http.StatusUnprocessableEntity)
+		return
+	}
+	block := n.chain.NextBlock([]byte(pay.Data))
+	w.Header().Set("Location", fmt.Sprintf("%s/blocks/%d", req.Host, block.Index))
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(201)
+	json.NewEncoder(w).Encode(block)
 }
